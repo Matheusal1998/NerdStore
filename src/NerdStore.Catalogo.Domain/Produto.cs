963bfe2f6e246e1aeded1464bceb3207240a030e
Matheus Lima <matheus.al1998@gmail.com>
2022-06-08 20:10:35 -0300

    [FEATURE] Extraindo classe Categoria

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -58,23 +58,6 @@ namespace NerdStore.Catalogo.Domain
         }
 
     }
-    public class Categoria : Entity
-    {
-        public string Nome { get; private set; }
-        public int Codigo { get; private set; }
-
-        public Categoria(string nome, int codigo)
-        {
-            Nome = nome;
-            Codigo = codigo;
-        }
-
-        public override string ToString()
-        {
-            return $"{Nome} - {Codigo}";
-        }
-
-    }
 
    
 }
