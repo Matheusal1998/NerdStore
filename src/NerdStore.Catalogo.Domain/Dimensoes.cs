a977ef0dadb107ee72c1231fe71b2150ce6a3357
Matheus Lima <matheus.al1998@gmail.com>
2022-06-11 13:30:30 -0300

    [FEATURE] Criando projeto de tests e criando tests para produtos

--- src/NerdStore.Catalogo.Domain/Dimensoes.cs
+++ src/NerdStore.Catalogo.Domain/Dimensoes.cs
@@ -13,9 +13,9 @@ namespace NerdStore.Catalogo.Domain
 
         public Dimensoes(decimal altura, decimal largura, decimal profundidade)
         {
-            Validacoes.ValidarSeMenorQue(altura, 1, "O campo altura deve ser maior que um");
-            Validacoes.ValidarSeMenorQue(largura, 1, "O campo largura deve ser maior que um");
-            Validacoes.ValidarSeMenorQue(profundidade, 1, "O campo profunidade deve ser maior que um");
+            Validacoes.ValidarSeMenorQue(altura, 1, "O campo Altura não pode ser menor ou igual a 0");
+            Validacoes.ValidarSeMenorQue(largura, 1, "O campo Largura não pode ser menor ou igual a 0");
+            Validacoes.ValidarSeMenorQue(profundidade, 1, "O campo Profundidade não pode ser menor ou igual a 0");
 
             Altura = altura;
             Largura = largura;
